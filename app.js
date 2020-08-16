9432abd8c04251630c67577113fe2553f864d0e2
Ryan Durk <rpdurk@gmail.com>
2020-08-16 15:38:35 -0700

    added a prevent default to ensure information can be loaded to DOM.  Single City search now works

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     // $(".weather").hide();
     const APIKey = "84e00228b4110f0bb695057ef871a2b0";
     // create function that begins on click
-    $(".search").on("submit", function () {
+    $(".search").on("submit", function (event) {
+        event.preventDefault()
         // set variables for easier ability to create values
         // $(".weather").show();
         var city = $("#city").val();
@@ -37,22 +38,26 @@ $(document).ready(function() {
         // UV index - states severity of the UV Index by color warning
         
         // Transfer content to dynamically
-            // var $card = $("<div>").addClass("card card-cascade wider reverse my-4 pb-5");
-            
-            let $searchCityDiv = $("#searchCityDiv");
+            let $searchCityDiv = $("<div>")//$("#searchCityDiv");
             $searchCityDiv.addClass("container-fluid text-center").attr("id", "searchCityDiv");
+            let $card = $("<div>");
+            $card.addClass("card card-cascade wider reverse my-4 pb-5");
             let $cardBody = $("<div>");
             $cardBody.addClass("card-body card-body-cascade text-center wow fadeIn cityWeather").attr("data-wow-delay", "0.2s");
             let $cityName = $("<h3>");
             $cityName.addClass("card-title cityName").text(`City: ${city}`);
+            // let $todaysDate = $("<h5>");
+            // $todaysDate.addClass("card-title date").text(` ${todaysDate}`);
             let $temp = $("<p>");
-            $temp.addClass("card-text temp").text(`Humidity: ${humidity}%`);
+            $temp.addClass("card-text temp").text(`Temperature: ${temp.toFixed(2)} F`);
             let $humidity = $("<p>");
             $humidity.addClass("card-text humidity").text(`Humidity: ${humidity}%`);
             let $wind = $("<p>");
             $wind.addClass("card-text wind").text(`Wind Speed: ${wind} m/h`);
-            // $cardBody.append($("<h4>").addClass("card-title date").text(date));
-            $cardBody.append($cityName, $temp, $humidity, $wind);
+            // let $uvIndex = $("<p>");
+            // $uvIndex.addClass("card-title uvIndex").text(`UV Index ${uvIndex}`);
+            $card.append($cityName, /*$todaysDate, */ $temp, $humidity, $wind, /*$uvIndex, */);
+            $cardBody.append($card);
             $searchCityDiv.append($cardBody);
             $('.cityContainer').append($searchCityDiv);
                 console.log($cardBody);
@@ -60,31 +65,19 @@ $(document).ready(function() {
                 console.log("Wind Speed: " + wind);
                 console.log("Humidity: " + humidity);
                 console.log("Temperature (F): " + temp);
-        // $(".date").text(todaysDate);
-        // $(".temp").text(temp);
-        // $(".humidity").text(humidity);
-        // $(".wind").text(wind);
-        // Convert the temp to fahrenheit
-        // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        // add temp content to html
-        // $(".temp").text("Temperature (K) " + response.main.temp);
-        // $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-        // Log the data in the console as well
-        
-        }); 
+        });
         // 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-
         // search history
         // current and future conditions for that city
         // open the weather dashboard
-        // THEN I am presented with the last searched city forecast
-
-
+ 
+ 
+ 
+ // THEN I am presented with the last searched city forecast
         // Example from Artist
         // // Add code to query the bands in town api searching for the artist received as an argument to this function
         // var appKey = "84e00228b4110f0bb695057ef871a2b0";
         // const $artistDiv = $('#artist-div');
-
         // $.ajax({
         // url: "https://rest.bandsintown.com/artists/" + artist + "?app_id=" + appKey,
         // method: "GET"
@@ -95,6 +88,6 @@ $(document).ready(function() {
         // $artistDiv.append($('<h1>').append("Number of fans tracking the " + artist + " " + response.tracker_count));
         // $artistDiv.append($('<h1>').append("Number of upcoming events =" + " " + response.upcoming_event_count));
         // $artistDiv.append($('<a>').attr("href", response.url).append(artist));
-        // });   
+        // });  
     });    
-});
+ });
