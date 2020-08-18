07f21ea8bbd2bd3b1a2a058f4f80d4e24b6f5218
Ryan Durk <rpdurk@gmail.com>
2020-08-17 22:31:59 -0700

    Formated js for easier reading

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {    
+$(document).ready(function () {
     // API key stored in a variable for easier access
     $(".forecast").hide();
     // place to store Locations for local storage
@@ -16,7 +16,7 @@ $(document).ready(function() {
             let $buttonGroup = $("<div>");
             $buttonGroup.addClass("btn-group mr-2").attr("role", "group").attr("aria-label", "First group");
             let $searchedCityBtn = $("<btn>");
-            $searchedCityBtn.attr("type", "button").attr("data-city" , [i]).addClass("btn btn-info").text(locations);
+            $searchedCityBtn.attr("type", "button").attr("data-city", [i]).addClass("btn btn-info locationHistory").text(locations);
             $buttonGroup.append($searchedCityBtn);
             $buttonToolbar.append($buttonGroup);
             $(".mainSearch").append($buttonToolbar);
@@ -24,13 +24,13 @@ $(document).ready(function() {
     }
     // Checks Local Storage
     function init() {
-        let storedLocations =JSON.parse(localStorage.getItem("locations"));
-     if (storedLocations !== null) {
-        locations = storedLocations;
-    }
+        let storedLocations = JSON.parse(localStorage.getItem("locations"));
+        if (storedLocations !== null) {
+            locations = storedLocations;
+        }
         renderSearchedCities();
     };
-    
+
     const APIKey = "84e00228b4110f0bb695057ef871a2b0";
     // create function that begins on click
     $(".search").on("submit", function (event) {
@@ -40,14 +40,16 @@ $(document).ready(function() {
 
         // adds search history to buttons
         let locationsHistory = $("#city").val();
-        if (locationsHistory === ""){
+        if (locationsHistory === "") {
             return;
         };
         locations.push(locationsHistory);
         localStorage.setItem("locations", JSON.stringify(locations));
-            //*************************************** */ if possible, add country and zip code!
+        //*************************************** */ if possible, add country and zip code!
         // let country =$("#countryChoice").val();
         // let zip =$("#zip").val();
+
+
         // the URL stored in a variable for easier access
         var queryURLWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`
         // Do an ajax request using the URL variable
@@ -55,71 +57,71 @@ $(document).ready(function() {
             url: queryURLWeather,
             method: "GET"
             // Store all of the retrieved data inside of an object called "response"
-        }).then (function(response) {
+        }).then(function (response) {
             // create several variables which will need to be called
-                // city response
-                var cityName = response.name;
-                // todays date
-                // var todaysDate = moment().format("MMM Do YY").val();
-                // temp in F.
-                var temp = (parseInt(response.main.temp) - 273.15) * 1.80 + 32;
-                // humidity
-                var humidity = response.main.humidity;
-                // wind speed
-                var wind = response.wind.speed;
-               const lon = response.coord.lon;
-               const lat = response.coord.lat;
-               var uvIndexValue =  uvIndexFunction(lat, lon);
-               console.log(uvIndexValue);
+            // city response
+            var cityName = response.name;
+            // todays date
+            // var todaysDate = moment().format("MMM Do YY").val();
+            // temp in F.
+            var temp = (parseInt(response.main.temp) - 273.15) * 1.80 + 32;
+            // humidity
+            var humidity = response.main.humidity;
+            // wind speed
+            var wind = response.wind.speed;
+            const lon = response.coord.lon;
+            const lat = response.coord.lat;
+            var uvIndexValue = uvIndexFunction(lat, lon);
+            console.log(uvIndexValue);
             // UV index - states severity of the UV Index by color warning
-            
+
             // Transfer content to dynamically
-                let $searchCityDiv = $("<div>")//$("#searchCityDiv");
-                $searchCityDiv.addClass("container-fluid text-center").attr("id", "searchCityDiv");
-                let $card = $("<div>");
-                $card.addClass("card card-cascade wider reverse my-4 pb-5 weatherFacts");
-                let $cardBody = $("<div>");
-                $cardBody.addClass("card-body card-body-cascade text-center wow fadeIn cityWeather").attr("data-wow-delay", "0.2s");
-                let $cityName = $("<h3>");
-                $cityName.addClass("card-title cityName").text(`City: ${cityName}`);
-                // let $todaysDate = $("<h5>");
-                // $todaysDate.addClass("card-title date").text(` ${todaysDate}`);
-                let $temp = $("<p>");
-                $temp.addClass("card-text temp").text(`Temperature: ${temp.toFixed(2)} F`);
-                let $humidity = $("<p>");
-                $humidity.addClass("card-text humidity").text(`Humidity: ${humidity}%`);
-                let $wind = $("<p>");
-                $wind.addClass("card-text wind").text(`Wind Speed: ${wind} m/h`);
-                // let $uvIndex = $("<p>");
-                // $uvIndex.addClass("card-text uvIndex"); 
-                // $uvIndex.addClass("card-text uvIndex").text(`UV Index ${uvIndexValue}`); ***********original Line*****
-                $card.append($cityName, /*$todaysDate, */ $temp, $humidity, $wind, /*$uvIndex*/);
-                $cardBody.append($card);
-                $searchCityDiv.append($cardBody);
-                $('.cityContainer').append($searchCityDiv);
-                    // console.log("City: " + city);
-                    // console.log("Wind Speed: " + wind);
-                    // console.log("Humidity: " + humidity);
-                    // console.log("Temperature (F): " + temp);
-                    // console.log(lon);
-                    // console.log(lat);
+            let $searchCityDiv = $("<div>")//$("#searchCityDiv");
+            $searchCityDiv.addClass("container-fluid text-center").attr("id", "searchCityDiv");
+            let $card = $("<div>");
+            $card.addClass("card card-cascade wider reverse my-4 pb-5 weatherFacts");
+            let $cardBody = $("<div>");
+            $cardBody.addClass("card-body card-body-cascade text-center wow fadeIn cityWeather").attr("data-wow-delay", "0.2s");
+            let $cityName = $("<h3>");
+            $cityName.addClass("card-title cityName").text(`City: ${cityName}`);
+            // let $todaysDate = $("<h5>");
+            // $todaysDate.addClass("card-title date").text(` ${todaysDate}`);
+            let $temp = $("<p>");
+            $temp.addClass("card-text temp").text(`Temperature: ${temp.toFixed(2)} F`);
+            let $humidity = $("<p>");
+            $humidity.addClass("card-text humidity").text(`Humidity: ${humidity}%`);
+            let $wind = $("<p>");
+            $wind.addClass("card-text wind").text(`Wind Speed: ${wind} m/h`);
+            // let $uvIndex = $("<p>");
+            // $uvIndex.addClass("card-text uvIndex"); 
+            // $uvIndex.addClass("card-text uvIndex").text(`UV Index ${uvIndexValue}`); ***********original Line*****
+            $card.append($cityName, /*$todaysDate, */ $temp, $humidity, $wind, /*$uvIndex*/);
+            $cardBody.append($card);
+            $searchCityDiv.append($cardBody);
+            $('.cityContainer').append($searchCityDiv);
+            // console.log("City: " + city);
+            // console.log("Wind Speed: " + wind);
+            // console.log("Humidity: " + humidity);
+            // console.log("Temperature (F): " + temp);
+            // console.log(lon);
+            // console.log(lat);
             // Attempt to have local storage
 
 
             // Attempt to render searched cities onto the screen
-                // let $buttonToolbar = $("<div>");
-                // $buttonToolbar.addClass("btn-toolbar justify-content-center").attr("role", "toolbar").attr("aria-label", "Toolbar with button groups");
-                // let $buttonGroup = $("<div>");
-                // $buttonGroup.addClass("btn-group mr-2").attr("role", "group").attr("aria-label", "First group");
-                // let $searchedCityBtn = $("<btn>");
-                // $searchedCityBtn.attr("type", "button").attr("data-city" , `${cityName}`).addClass("btn btn-info").text(`${cityName}`)
-                // $buttonGroup.append($searchedCityBtn);
-                // $buttonToolbar.append($buttonGroup);
-                // $(".mainSearch").append($buttonToolbar);
-                // Check local storage
-                // if ();
+            // let $buttonToolbar = $("<div>");
+            // $buttonToolbar.addClass("btn-toolbar justify-content-center").attr("role", "toolbar").attr("aria-label", "Toolbar with button groups");
+            // let $buttonGroup = $("<div>");
+            // $buttonGroup.addClass("btn-group mr-2").attr("role", "group").attr("aria-label", "First group");
+            // let $searchedCityBtn = $("<btn>");
+            // $searchedCityBtn.attr("type", "button").attr("data-city" , `${cityName}`).addClass("btn btn-info").text(`${cityName}`)
+            // $buttonGroup.append($searchedCityBtn);
+            // $buttonToolbar.append($buttonGroup);
+            // $(".mainSearch").append($buttonToolbar);
+            // Check local storage
+            // if ();
         });
-    });     
+    });
 
     function uvIndexFunction(lat, lon) {
         // event.preventDefault()
@@ -127,11 +129,11 @@ $(document).ready(function() {
         $.ajax({
             url: queryURLData,
             method: "GET"
-        }).then (function(data) {
+        }).then(function (data) {
             let uvIndex = data.current.uvi;
             // console.log("UV: " + uvIndex);
             let $uvIndex = $("<p>");
-            $uvIndex.addClass("card-text uvIndex"); 
+            $uvIndex.addClass("card-text uvIndex");
             $(".weatherFacts").append($uvIndex);
             // UV index - states severity of the UV Index by color warning
             if (uvIndex > 0.01 & uvIndex < 3) {
@@ -140,7 +142,7 @@ $(document).ready(function() {
             } else if (uvIndex > 3 & uvIndex < 6) {
                 // color turns yellow 
                 $uvIndex.addClass('yellow accent-1').text(`UV Index: Moderate to High Danger  ${uvIndex}`);
-            } else if (uvIndex > 6  & uvIndex < 8) {
+            } else if (uvIndex > 6 & uvIndex < 8) {
                 // color turns orange 
                 $uvIndex.addClass('warning-color').text(`UV Index: Moderate to High Danger  ${uvIndex}`);
             } else if (uvIndex > 8 & uvIndex < 11) {
@@ -152,7 +154,7 @@ $(document).ready(function() {
             }
             return uvIndex;
         });
-    }   
+    }
 
     // 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
     $(".search").on("submit", function (event) {
@@ -164,7 +166,7 @@ $(document).ready(function() {
         $.ajax({
             url: queryURLForecast,
             method: "GET"
-        }).then (function(forecast) {
+        }).then(function (forecast) {
             // console.log(forecast);
             let forecastIndex = 0;
             // Create a for loop to dynamically render all 5 days
@@ -176,12 +178,12 @@ $(document).ready(function() {
                 let forecastDate = forecast.list[forecastIndex].dt_txt;
                 let forecastTemp = (parseInt(forecast.list[forecastIndex].main.temp) - 273.15) * 1.80 + 32;
                 let forecastHumidity = forecast.list[forecastIndex].main.humidity;
-                
+
                 // console.log(forecast.list[forecastIndex].weather.icon);
                 // console.log(forecastWeatherURL);
                 // console.log(forecastIconDescription);
-            
-            //  need to be added each time! 
+
+                //  need to be added each time! 
                 let $forecastCol = $("<div>");
                 $forecastCol.addClass("col");
                 let $forecastCard = $("<div>");
@@ -192,9 +194,9 @@ $(document).ready(function() {
                 $forecastDate.addClass("card-title text-white").text(forecastDate);
                 let $forecastWeatherIconLink = $('<span>');
                 $forecastWeatherIconLink.append($('<img>').attr('src', forecastWeatherURL));
-                let $forecastIconDescription= $("<p>");
+                let $forecastIconDescription = $("<p>");
                 $forecastIconDescription.addClass("card-text text-white").text(`${forecastIconDescription}`);
-                let $forecastTemp= $("<p>");
+                let $forecastTemp = $("<p>");
                 $forecastTemp.addClass("card-text text-white").text(`Temperature: ${forecastTemp.toFixed(2)} F`);
                 let $forecastHumidity = $("<p>");
                 $forecastHumidity.addClass("card-text text-white").text(`Humidity: ${forecastHumidity}%`);
@@ -215,5 +217,10 @@ $(document).ready(function() {
             // $forecastTitle.append($forecastDiv);
             // $forecastRow.append($forecastTitle);
         });
-    });    
+    });
+
+    // $(".locationHistory").on("click", { 
+    //     let locations = cities
+    //     search();
+    // });
 });
